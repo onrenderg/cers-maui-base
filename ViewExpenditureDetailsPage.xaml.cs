6540f44b330247edaeb736c950a43a54cba1e872
Your Name <your_email@example.com>
2025-10-14 17:24:35 +0530

    date chage fmt

--- ViewExpenditureDetailsPage.xaml.cs
+++ ViewExpenditureDetailsPage.xaml.cs
@@ -205,8 +205,11 @@ namespace CERS
                         $", (case when ObserverRemarks <> '' then 'true' else 'false' end)btnrplyobserverremarksvisibility" +
                         $",'false' as exptypevisibility" +
                         $",'true' as expdatevisibility" +
-                        $",CASE WHEN expDate IS NOT NULL THEN date(expDate) ELSE '' END as expDateDisplay" +
-                        $",CASE WHEN paymentDate IS NOT NULL THEN date(paymentDate) ELSE '' END as paymentDateDisplay" +
+                        //$",CASE WHEN expDate IS NOT NULL THEN date(expDate) ELSE '' END as expDateDisplay" +
+                        //$",CASE WHEN paymentDate IS NOT NULL THEN date(paymentDate) ELSE '' END as paymentDateDisplay" +
+                        $",CASE WHEN expDate IS NOT NULL THEN strftime('%d-%m-%Y', expDate) ELSE '' END as expDateDisplay" +
+                        $",CASE WHEN paymentDate IS NOT NULL THEN strftime('%d-%m-%Y', paymentDate) ELSE '' END as paymentDateDisplay" + 
+
                         $",CASE WHEN evidenceFile = 'Y' THEN 'true' ELSE 'false' END as pdfvisibility" +
                         $" from ExpenditureDetails " +
                         $" where expcode='{expvalue}'";
